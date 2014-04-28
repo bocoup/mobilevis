1f82cc468f77d9ec68e129fe112869f14a2c1bbc
Irene Ros <imirene@gmail.com>
2014-04-28 14:13:53 -0400

    Added passport integration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const config = require('./config/api');
+const session = require('./config/session');
+
 const express = require('express');
 const routeBuilder = require('express-routebuilder');
 const Inflector = require('inflection');
+
+var P = require('./app/classes/passport');
 
 const resources = [
   'submission',
@@ -10,10 +14,30 @@ const resources = [
 
 var app = express();
 
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.methodOverride());
+
+// body parsing
 app.use(express.json());
 app.use(express.urlencoded());
+
+// allow cross domain
 app.use(require('./app/middlewares/cors'));
+
+// route debugging
 app.use(require('./app/middlewares/debug'));
+
+// session secret key
+app.use(express.session({ secret: session.secret }));
+
+// add passport integration
+var passport = P.init();
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(routeBuilder(express, require('./app/routes/passport')(passport)));
 
 if(!process.env.testing) {
   resources.forEach(function (resource) {
