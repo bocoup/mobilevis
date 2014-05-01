871a6576f581ee3c9bb6f41cc945410e85f34c46
Irene Ros <imirene@gmail.com>
2014-05-01 16:26:52 -0400

    setting correct content type headers on s3 uploader

--- api/app/classes/s3.js
+++ api/app/classes/s3.js
@@ -3,18 +3,25 @@ const knox = require('knox');
 const MultiPartUpload = require('knox-mpu');
 const when = require('when');
 const _ = require('lodash');
+const mime = require('mime');
 
 exports.config = config;
 exports.client = knox.createClient(config);
 
-exports.writeStream = function (stream, headers) {
+exports.writeStream = function (stream, filename, headers) {
   var deferred = when.defer();
   var uuid = require('node-uuid').v4();
+
+  console.log("content type", mime.lookup(filename));
+
   var params = {
     client: exports.client,
     objectName: config.prefix+uuid,
     stream: stream,
-    headers: _.extend({ 'x-amz-acl': 'public-read' }, headers)
+    headers: _.extend({
+      'x-amz-acl': 'public-read',
+      'Content-Type': mime.lookup(filename)
+    }, headers)
   };
   new MultiPartUpload(params, function (err) {
     if (err) {
