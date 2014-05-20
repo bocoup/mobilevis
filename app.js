4b3ec6632084f8b4468c3cf3c0c60b6f4104920a
Irene Ros <imirene@gmail.com>
2014-05-20 16:42:57 -0400

    getting nginx setup to work

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const host = process.env.HOST || config.host;
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-app.use('/public', express.static('public'));
-app.use('/src', express.static('src'));
-app.use('/bower_components', express.static('bower_components'));
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/src', express.static(__dirname + '/src'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.use(require('./api'));
+app.use(require(__dirname + '/api'));
 
 app.get('*', function (req, res) {
-  res.sendfile('./public/index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(port, host);
