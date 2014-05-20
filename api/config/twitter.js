09aef73e393428712bb419c997aab9ec5dc23641
Irene Ros <imirene@gmail.com>
2014-05-20 15:49:12 -0400

    fixing postgres related db access stuff

--- api/config/twitter.js
+++ api/config/twitter.js
@@ -1,5 +1,5 @@
 module.exports = {
   CONSUMER_KEY : 'oNbrdguWnQzITsMLawjXJw',
   CONSUMER_SECRET : '8qRE0B9qYAmqqUsAbCXYFckH7k45fp3upu2bbVkyibk',
-  redirect_host: "http://mobilevis.bocoup.com:8000"
+  redirect_host: "http://mobilevis.bocoup.com"
 };
