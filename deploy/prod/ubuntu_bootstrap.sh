e54bf43fefddb9f2ddd665bae41be56f02f95e50
Irene Ros <imirene@gmail.com>
2014-05-21 12:52:45 -0400

    new dev vs prod deploy process

---
+++ deploy/prod/ubuntu_bootstrap.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+# This script can be copied and pasted onto an Ubuntu 12.04 machine and run, it's interactive, but only at the beginning
+# When finished the admiralty application should be setup and responding to requests on port 80 and 8000
+# Eventually this should be refactored to leverage CM tool for now relative ease of use is priority
+
+echo ">>> Running prod specific setup"
+
+# annoying ssh-add ubuntu bug, have to "turn on" the auth agent
+eval "$(ssh-agent)"
+
+# add our key to the ssh agent (setup in vagrant file.)
+ssh-add ~/.ssh/id_rsa
+
+# add github to hosts
+echo "Host gh
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
+
+# Add github to known hosts so we aren't prompted when cloning
+ssh-keyscan -H github.com | sudo tee /etc/ssh/ssh_known_hosts
+
+# checkout source code into our working directory
+cd /vagrant
+git clone git@github.com:bocoup/mobilevis.git
+
+# Backup and Restore Scripts
+sudo ln -s /vagrant/deploy/prod/backup/mobilevis-backup /usr/local/bin/mobilevis-backup
+chmod 777 /vagrant/deploy/prod/backup/mobilevis-backup
+sudo ln -s /vagrant/deploy/prod/backup/mobilevis-restore /usr/local/bin/mobilevis-restore
+chmod 777 /vagrant/deploy/prod/backup/mobilevis-restore
+
+# NGINX Conf - change this file to eventually point at production url!
+sudo ln -s /vagrant/deploy/prod/nginx/mobilevis.conf /etc/nginx/conf.d/mobilevis.conf
