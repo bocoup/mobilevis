5f510f5629c89ca89b7378a9e3b1fe00ce0b9d9d
Irene Ros <imirene@gmail.com>
2014-05-26 16:36:11 -0400

    Updating migrations to set primary key and on cascade delete

--- api/db/migrations/005-comments.js
+++ api/db/migrations/005-comments.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('comments', function (t) {
-    t.increments('id');
+    t.increments('id').primary();
     t.text('twitter_handle').notNullable();
     t.text('comment').notNullable();
-    t.integer('submission_id').references('id').inTable('submissions').notNullable();
+    t.integer('submission_id').references('id').inTable('submissions').notNullable().onDelete('CASCADE');
     t.timestamp('timestamp').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
   });
 };
